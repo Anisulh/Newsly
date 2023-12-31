49bf6082e6a1e0d572e7608f03ee15fb0524656b
Anisul <anisulhaque9391@gmail.com>
2023-12-31 15:09:46 -0500

    feat: centralized error handling

---
+++ server/middleware/errorhandler.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+    "github.com/gofiber/fiber/v2"
+    "log"
+)
+
+func ErrorHandler(ctx *fiber.Ctx, err error) error {
+    // Log the error
+    log.Printf("Error: %v", err)
+
+    // Determine error type and send appropriate response
+    code := fiber.StatusInternalServerError
+    msg := "Internal Server Error"
+
+    if e, ok := err.(*fiber.Error); ok {
+        code = e.Code
+        msg = e.Message
+    }
+
+    return ctx.Status(code).JSON(fiber.Map{
+        "error": msg,
+    })
+}
