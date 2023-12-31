20927e79fff8e1c7186444d9688d490d139d4814
Anisul <anisulhaque9391@gmail.com>
2023-12-31 15:11:37 -0500

    feat: added util to generate Jwt

---
+++ server/utils/generateJWT.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+func GenerateJWT(id uint) (string, error) {
+	jwtSecret := []byte(os.Getenv("JWTSECRET"))
+if len(jwtSecret) == 0 {
+	return "", errors.New("JWT secret is not set")
+}
+// Create token
+token := jwt.New(jwt.SigningMethodHS256)
+
+// Set claims
+claims := token.Claims.(jwt.MapClaims)
+claims["user_id"] = id
+claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+
+// Generate encoded token and send it as response.
+t, err := token.SignedString(jwtSecret)
+if err != nil {
+	return "", err
+}
+
+return t, nil
+}
