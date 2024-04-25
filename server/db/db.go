99e1279de9a904edb347f3988a59e34d6ca0bfc4
ubergoose <anisulhaque9391@gmail.com>
2024-04-25 14:50:05 -0400

    Refactor database setup and migration logic in db.go

--- server/db/db.go
+++ server/db/db.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/Anisulh/content_personalization/models"
 	"github.com/Anisulh/content_personalization/utils"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -11,6 +12,14 @@ func SetupDatabase() (*gorm.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-
+	err = Migrate(db)
+	if err != nil {
+		return nil, err
+	}
 	return db, nil
+}
+
+func Migrate(db *gorm.DB) error {
+	// Migrate the schema
+	return db.AutoMigrate(&models.Keyword{}, &models.User{}, &models.Content{} )
 }
