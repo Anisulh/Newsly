329712b83c557e18b154d73ceb262f6111a6ea69
ubergoose <anisulhaque9391@gmail.com>
2024-04-25 14:50:24 -0400

    Refactor category and content models in server/models

--- server/models/contentModel.go
+++ server/models/contentModel.go
@@ -6,13 +6,26 @@ type Content struct {
 	gorm.Model
 	Title       string
 	Description string
-	URL         string
+	Content     string
+	URL         string `gorm:"uniqueIndex"`
 	ImageURL    string
 	PublishedAt string
 	Source      string
-	CategoryID  uint
-	Category    string //Category
-	Keywords		[]string
-	Likes       []User `gorm:"many2many:user_likes;"`
-	Dislikes    []User `gorm:"many2many:user_dislikes;"`
+	Category    Category
+	Keywords    []Keyword `gorm:"many2many:content_keywords;"`
+	Likes       []*User   `gorm:"many2many:user_likes;"`
+	Dislikes    []*User   `gorm:"many2many:user_dislikes;"`
 }
+
+// Category is an enumerated type for content categories.
+type Category string
+
+const (
+	World         Category = "World"
+	Politics      Category = "Politics"
+	Technology    Category = "Technology"
+	Science       Category = "Science"
+	Entertainment Category = "Entertainment"
+	Business      Category = "Business"
+	Sports        Category = "Sports"
+)
