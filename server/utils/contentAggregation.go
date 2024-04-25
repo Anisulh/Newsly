1adaee9175c628b9e9178ca892aaf102ece79c5e
ubergoose <anisulhaque9391@gmail.com>
2024-04-25 14:50:54 -0400

    Add additional functions such as fetch log news and store news

--- server/utils/contentAggregation.go
+++ server/utils/contentAggregation.go
@@ -3,6 +3,7 @@ package utils
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -10,8 +11,25 @@ import (
 	"time"
 
 	"github.com/Anisulh/content_personalization/models"
-	"github.com/IBM/sarama"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"gorm.io/gorm"
 )
+
+type Content struct {
+	ID          uint       `json:"ID"`
+	CreatedAt   time.Time  `json:"CreatedAt"`
+	UpdatedAt   time.Time  `json:"UpdatedAt"`
+	DeletedAt   *time.Time `json:"DeletedAt"`
+	Title       string     `json:"Title"`
+	Description string     `json:"Description"`
+	Content     string     `json:"Content"`
+	URL         string     `json:"URL"`
+	ImageURL    string     `json:"ImageURL"`
+	PublishedAt string     `json:"PublishedAt"`
+	Source      string     `json:"Source"`
+	Keywords    []string   `json:"Keywords"`
+	Category    string     `json:"Category"`
+}
 
 // Struct to parse response from NewsAPI
 type NewsAPIResponse struct {
@@ -29,102 +47,210 @@ type NewsAPIResponse struct {
 	} `json:"articles"`
 }
 
-type MyData struct {
-	FirstElement string
-	SecondElement []string
-}
+const KafkaTopicNewsInput = "news-input"
+const KafkaConsumerGroup = "news-consumer-group"
 
-func CallNLP(text string) (MyData, error) {
-	// function that calls the nlp via grpc sending the content and recieving an array ['category', ['keywords']]
-	return MyData{FirstElement: "", SecondElement: []string{"", "", ""}}, nil
-}
-
-func produceToKafka(topic string, message []byte, brokers []string) error {
-	config := sarama.NewConfig()
-	config.Producer.Return.Successes = true
-
-	producer, err := sarama.NewSyncProducer(brokers, config)
-	if err != nil {
-			return err
-	}
-	defer producer.Close()
-
-	_, _, err = producer.SendMessage(&sarama.ProducerMessage{
-			Topic: topic,
-			Value: sarama.ByteEncoder(message),
-	})
-	return err
-}
-
-func FetchNews() ([]models.Content, error) {
-
+func FetchNews(db *gorm.DB) error {
+	log.Println("Fetching news")
 	url := fmt.Sprintf("https://newsapi.org/v2/top-headlines?country=us&apiKey=%v", NewsAPIKey)
-	resp, err := http.Get(url)
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+	resp, err := client.Get(url)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer resp.Body.Close()
 
+	// Check HTTP Response
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	var apiResp NewsAPIResponse
 	if err := json.Unmarshal(body, &apiResp); err != nil {
-		return nil, err
+		return err
 	}
 
-	var contents []models.Content
-	for _, article := range apiResp.Articles {
-		val, err := CallNLP(article.Content)
-		if err != nil {
-			return nil, err
-		}
-		fmt.Print(val)
-		
+	// Kafka writer initialization (outside the loop)
+	producer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost:9092"})
+	if err != nil {
+		return err
+	}
+	defer producer.Close()
 
-		// Serialize content for Kafka
-		contentBytes, err := json.Marshal(models.Content{
+	topic := "news-input"
+	log.Println("Looping through articles")
+
+	for _, article := range apiResp.Articles {
+		content := models.Content{
 			Title:       article.Title,
 			Description: article.Description,
+			Content:     article.Content,
 			URL:         article.Url,
 			ImageURL:    article.UrlToImage,
 			PublishedAt: article.PublishedAt,
-			Source:      article.Source.Id,
-			Category:    val.FirstElement,
-			Keywords:    val.SecondElement,
-	})
-	if err != nil {
-			return nil, err
-	}
-	// Produce the message to Kafka
-	err = produceToKafka("news_topic", contentBytes, []string{"localhost:9092"})
-	if err != nil {
-			log.Printf("Failed to produce message to Kafka: %s", err)
-	}
-		
-	}
-	// return to datastorage function to save to db
-	return contents, nil
-}
+			Source:      article.Source.Name,
+		}
 
+		// Check if the content already exists in the db
+		var existingContent models.Content
+		log.Printf("Checking for URL: %s", article.Url)
+		result := db.Model(&models.Content{}).Where("url = ?", article.Url).First(&existingContent)
 
-func StartScheduledFetching() {
-	ticker := time.NewTicker(24 * time.Hour) // Set the duration according to your needs
+		if result.Error != nil && !errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			log.Printf("Error querying db: %v", result.Error)
+			continue // Skip this article
+		}
 
-	go func() {
-			for {
-					select {
-					case <-ticker.C:
-							_, err := FetchNews()
-							if err != nil {
-									log.Printf("Error fetching news: %s", err)
-							} else {
-									log.Println("Successfully fetched news")
-							}
-					}
+		// If the content does not exist, send it to Kafka
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			contentBytes, err := json.Marshal(content)
+			if err != nil {
+				return err
 			}
-	}()
+
+			// Kafka production code...
+			if err := producer.Produce(&kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+				Value:          contentBytes,
+			}, nil); err != nil {
+				log.Printf("Error producing Kafka message: %v", err)
+			}
+		} else {
+			log.Println("Article exists in db")
+		}
+	}
+
+	producer.Flush(15 * 1000)
+	return nil
 }
 
+func StartScheduledFetching(db *gorm.DB) {
+	ticker := time.NewTicker(24 * time.Hour)
+	defer ticker.Stop()
+
+	// Run the task once immediately and then schedule it to run periodically
+	fetchAndLogNews(db)
+	
+	for range ticker.C {
+			fetchAndLogNews(db)
+	}
+}
+
+func fetchAndLogNews(db *gorm.DB) {
+	err := FetchNews(db)
+	if err != nil {
+			log.Printf("Error fetching news: %s", err)
+	} else {
+			log.Println("Successfully fetched news")
+	}
+}
+
+func StoreNews(db *gorm.DB) {
+	log.Println("Starting to consume messages from news-output topic...")
+
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"group.id":          "news-consumer-group",
+		"auto.offset.reset": "earliest",
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to create consumer: %v", err)
+	}
+	defer consumer.Close()
+
+	consumer.SubscribeTopics([]string{"news-output"}, nil)
+
+	for {
+		msg, err := consumer.ReadMessage(-1)
+		if err != nil {
+			log.Printf("Error while reading message: %v", err)
+			continue
+		}
+
+		var content Content
+		err = json.Unmarshal(msg.Value, &content)
+		if err != nil {
+			log.Printf("Error unmarshaling message: %v", err)
+			continue
+		}
+
+		// Optimized Keyword Handling
+		existingKeywords := make(map[string]models.Keyword)
+		db.Find(&existingKeywords)
+		keywordModels := make([]models.Keyword, 0)
+		for _, keywordStr := range content.Keywords {
+			keyword, exists := existingKeywords[keywordStr]
+			if !exists {
+				keyword = models.Keyword{Name: keywordStr}
+				db.Create(&keyword)
+			}
+			keywordModels = append(keywordModels, keyword)
+		}
+
+		// Create a new instance of models.Content and populate it
+		modelsContent := models.Content{
+			Title:       content.Title,
+			Description: content.Description,
+			Content:     content.Content,
+			URL:         content.URL,
+			ImageURL:    content.ImageURL,
+			PublishedAt: content.PublishedAt,
+			Source:      content.Source,
+			Category:    models.Category(content.Category), // Convert to your Category type
+			Keywords:    keywordModels,
+		}
+
+		// Use Transactions for DB operations
+		tx := db.Begin()
+		defer func() {
+			if r := recover(); r != nil {
+				tx.Rollback()
+			}
+		}()
+		if err := tx.Error; err != nil {
+			log.Printf("Error starting transaction: %v", err)
+			return
+		}
+
+		// Check if the content already exists in the db
+		var existingContent models.Content
+		if err := tx.Where("url = ?", content.URL).First(&existingContent).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				// URL does not exist, safe to insert
+				if err := tx.Create(&modelsContent).Error; err != nil {
+					tx.Rollback()
+					log.Printf("Error saving content to db: %v", err)
+					return
+				}
+			} else {
+				// Handle other possible errors
+				tx.Rollback()
+				log.Printf("Error querying for existing content: %v", err)
+				return
+			}
+		} else {
+			// URL already exists, handle accordingly
+			log.Println("Content with this URL already exists")
+			// Assuming content contains the new data
+			if err := tx.Model(&existingContent).Updates(modelsContent).Error; err != nil {
+				tx.Rollback()
+				log.Printf("Error updating existing content: %v", err)
+				return
+			}
+		}
+
+		if err := tx.Commit().Error; err != nil {
+			log.Printf("Transaction commit error: %v", err)
+		} else {
+			log.Printf("Content processed successfully: %v", modelsContent.ID)
+		}
+	}
+}
