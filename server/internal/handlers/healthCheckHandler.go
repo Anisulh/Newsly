23f482346940a3589b37213c4bc5a4346d44f5ea
ubergoose <anisulhaque9391@gmail.com>
2024-04-26 02:03:00 -0400

    Refactor healthCheckHandler.go to remove unused imports and struct

--- server/internal/handlers/healthCheckHandler.go
+++ server/internal/handlers/healthCheckHandler.go
@@ -2,16 +2,8 @@ package handlers
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/gorm"
 )
 
-type Handler struct {
-	DB *gorm.DB
-}
-
-func NewHandler(db *gorm.DB) *Handler {
-	return &Handler{DB: db}
-}
 
 func (h *Handler) HealthCheck(c *fiber.Ctx) error {
 	return c.SendStatus(fiber.StatusOK)
