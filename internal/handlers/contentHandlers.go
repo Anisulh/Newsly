82ac8354ab118f58ffaf0146e1ace238f7037a82
ubergoose <anisulhaque9391@gmail.com>
2024-05-13 15:18:08 -0400

    refactor: comment out unused content handling routes and functions

--- internal/handlers/contentHandlers.go
+++ internal/handlers/contentHandlers.go
@@ -1,96 +1,96 @@
 package handlers
 
-import (
-	"strconv"
+// import (
+// 	"strconv"
 
-	"Newsly/internal/models"
-	"Newsly/internal/utils"
-	"github.com/gofiber/fiber/v2"
-)
+// 	"Newsly/internal/models"
+// 	"Newsly/internal/utils"
+// 	"github.com/gofiber/fiber/v2"
+// )
 
-func (h *Handler) GetContent(c *fiber.Ctx) error {
-	var contents []models.Content
-	if err := h.DB.Find(&contents).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot fetch content"})
-	}
+// func (h *Handler) GetContent(c *fiber.Ctx) error {
+// 	var contents []models.Content
+// 	if err := h.DB.Find(&contents).Error; err != nil {
+// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot fetch content"})
+// 	}
 
-	return c.JSON(contents)
-}
+// 	return c.JSON(contents)
+// }
 
-func (h *Handler) GetContentCategories(c *fiber.Ctx) error {
-	var categories []models.Category
-	if err := h.DB.Find(&categories).Error; err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot fetch categories"})
-	}
+// func (h *Handler) GetContentCategories(c *fiber.Ctx) error {
+// 	var categories []models.Category
+// 	if err := h.DB.Find(&categories).Error; err != nil {
+// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Cannot fetch categories"})
+// 	}
 
-	return c.JSON(categories)
-}
+// 	return c.JSON(categories)
+// }
 
-func LikeContent(c *fiber.Ctx) error {
-	userID := c.Locals("userID").(uint)
-	contentID, err := strconv.ParseUint(c.Params("contentId"), 10, 32)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Invalid content id"})
-	}
+// func LikeContent(c *fiber.Ctx) error {
+// 	userID := c.Locals("userID").(uint)
+// 	contentID, err := strconv.ParseUint(c.Params("contentId"), 10, 32)
+// 	if err != nil {
+// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Invalid content id"})
+// 	}
 
-	interaction := utils.UserInteraction{
-		UserID:    userID,
-		ContentID: uint(contentID),
-		Type:      "like",
-	}
+// 	interaction := utils.UserInteraction{
+// 		UserID:    userID,
+// 		ContentID: uint(contentID),
+// 		Type:      "like",
+// 	}
 
-	if kafkaErr := utils.SendMessage([]string{"localhost:9092"}, "user-interactions", interaction); kafkaErr != nil {
-		// Handle error
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Unable to send kafka message"})
-	}
+// 	if kafkaErr := utils.SendMessage([]string{"localhost:9092"}, "user-interactions", interaction); kafkaErr != nil {
+// 		// Handle error
+// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Unable to send kafka message"})
+// 	}
 
-	return c.SendStatus(400)
-	// Rest of your handler logic
-	// ...
-}
+// 	return c.SendStatus(400)
+// 	// Rest of your handler logic
+// 	// ...
+// }
 
-func DislikeContent(c *fiber.Ctx) error {
-	userID := c.Locals("userID").(uint)
-	contentID, err := strconv.ParseUint(c.Params("contentId"), 10, 32)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Invalid content id"})
-	}
+// func DislikeContent(c *fiber.Ctx) error {
+// 	userID := c.Locals("userID").(uint)
+// 	contentID, err := strconv.ParseUint(c.Params("contentId"), 10, 32)
+// 	if err != nil {
+// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Invalid content id"})
+// 	}
 
-	interaction := utils.UserInteraction{
-		UserID:    userID,
-		ContentID: uint(contentID),
-		Type:      "dislike",
-	}
+// 	interaction := utils.UserInteraction{
+// 		UserID:    userID,
+// 		ContentID: uint(contentID),
+// 		Type:      "dislike",
+// 	}
 
-	if kafkaErr := utils.SendMessage([]string{"localhost:9092"}, "user-interactions", interaction); kafkaErr != nil {
-		// Handle error
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Unable to send kafka message"})
-	}
+// 	if kafkaErr := utils.SendMessage([]string{"localhost:9092"}, "user-interactions", interaction); kafkaErr != nil {
+// 		// Handle error
+// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Unable to send kafka message"})
+// 	}
 
-	return c.SendStatus(400)
-	// Rest of your handler logic
-	// ...
-}
+// 	return c.SendStatus(400)
+// 	// Rest of your handler logic
+// 	// ...
+// }
 
-func BookmarkContent(c *fiber.Ctx) error {
-	userID := c.Locals("userID").(uint)
-	contentID, err := strconv.ParseUint(c.Params("contentId"), 10, 32)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Invalid content id"})
-	}
+// func BookmarkContent(c *fiber.Ctx) error {
+// 	userID := c.Locals("userID").(uint)
+// 	contentID, err := strconv.ParseUint(c.Params("contentId"), 10, 32)
+// 	if err != nil {
+// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Invalid content id"})
+// 	}
 
-	interaction := utils.UserInteraction{
-		UserID:    userID,
-		ContentID: uint(contentID),
-		Type:      "bookmark",
-	}
+// 	interaction := utils.UserInteraction{
+// 		UserID:    userID,
+// 		ContentID: uint(contentID),
+// 		Type:      "bookmark",
+// 	}
 
-	if kafkaErr := utils.SendMessage([]string{"localhost:9092"}, "user-interactions", interaction); kafkaErr != nil {
-		// Handle error
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Unable to send kafka message"})
-	}
+// 	if kafkaErr := utils.SendMessage([]string{"localhost:9092"}, "user-interactions", interaction); kafkaErr != nil {
+// 		// Handle error
+// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Unable to send kafka message"})
+// 	}
 
-	return c.SendStatus(400)
-	// Rest of your handler logic
-	// ...
-}
+// 	return c.SendStatus(400)
+// 	// Rest of your handler logic
+// 	// ...
+// }
