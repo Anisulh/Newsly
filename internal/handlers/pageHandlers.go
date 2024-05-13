4ecc953d436a8c3b402708e60989ff2db4018433
ubergoose <anisulhaque9391@gmail.com>
2024-05-13 18:57:07 -0400

    refactor: update import name

--- internal/handlers/pageHandlers.go
+++ internal/handlers/pageHandlers.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
-	"Newsly/web/templates"
+	"Newsly/web/templates/pages"
 
 	"github.com/a-h/templ"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/adaptor"
 )
+
+
 
 func Render(c *fiber.Ctx, component templ.Component, options ...func(*templ.ComponentHandler)) error {
 	componentHandler := templ.Handler(component)
@@ -17,13 +19,13 @@ func Render(c *fiber.Ctx, component templ.Component, options ...func(*templ.Comp
 }
 
 func (h *Handler) GetHomePage(c *fiber.Ctx) error {
-	return Render(c, templates.HomePage())
+	return Render(c, pages.HomePage())
 }
 
 func (h *Handler) GetLoginPage(c *fiber.Ctx) error {
-	return Render(c, templates.LoginPage())
+	return Render(c, pages.LoginPage())
 }
 
 func (h *Handler) GetRegisterPage(c *fiber.Ctx) error {
-	return Render(c, templates.RegisterPage())
+	return Render(c, pages.RegisterPage())
 }
