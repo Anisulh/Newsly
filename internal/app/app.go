ceb34eb767fad3fc704336cc38497611a12e2c7e
ubergoose <anisulhaque9391@gmail.com>
2024-06-11 11:21:00 -0400

    refactor: Add private page routes for authenticated users

--- internal/app/app.go
+++ internal/app/app.go
@@ -13,6 +13,12 @@ import (
 )
 
 func newApp(db *gorm.DB, config *config.Config) (*fiber.App, error) {
+	// Handlers
+	handler := handlers.NewHandler(db, config.JWTSecret, config.Environment, config.NewsAPIKey)
+
+	// Middleware
+	middleware := middleware.NewMiddleware(db, config.JWTSecret, config.Environment)
+
 	app := fiber.New(fiber.Config{
 		ErrorHandler: middleware.ErrorHandler,
 	})
@@ -24,11 +30,8 @@ func newApp(db *gorm.DB, config *config.Config) (*fiber.App, error) {
 		AllowOrigins: config.ClientAddress,
 		AllowHeaders: "Authorization, Origin, Content-Type, Accept",
 	}))
-	app.Use(logger.New())
-	app.Use("/api/secure", middleware.JWTProtected(config.JWTSecret))
 
-	// Handlers
-	handler := handlers.NewHandler(db, config.JWTSecret)
+	app.Use(logger.New())
 
 	// health check
 	app.Get("/api/health-check", handler.HealthCheck)
@@ -38,16 +41,20 @@ func newApp(db *gorm.DB, config *config.Config) (*fiber.App, error) {
 	pageRouter.Get("/", handler.GetHomePage)
 	pageRouter.Get("/login", handler.GetLoginPage)
 	pageRouter.Get("/register", handler.GetRegisterPage)
-	// pageRouter.Get("/news", handler.GetNewsPage)
 	// pageRouter.Get("/news/:id", handler.GetNewsDetailPage)
 	// pageRouter.Get("/profile", handler.GetProfilePage)
 	// pageRouter.Get("/preferences", handler.GetPreferencesPage)
+
+	// Private Page Routes
+	privatePageRouter := app.Group("/auth", middleware.JWTProtected())
+	privatePageRouter.Get("/feed", handler.GetFeedPage)
 
 	// API Routes
 	// User Auth
 	userPublicRouter := app.Group("/api/user")
 	userPublicRouter.Post("/register", handler.UserRegistration)
 	userPublicRouter.Post("/login", handler.UserLogin)
+	userPublicRouter.Post("/logout", handler.UserLogout)
 
 	// Content Discover
 	// contentPublicRouter := app.Group("/api/content")
@@ -55,15 +62,16 @@ func newApp(db *gorm.DB, config *config.Config) (*fiber.App, error) {
 	// contentPublicRouter.Get("/categories", handler.GetContentCategories)
 
 	// Secured Routes
+	app.Use("/api/secure", middleware.JWTProtected())
 	// User Profile
-	userPrivateRouter := app.Group("/api/user", middleware.JWTProtected(config.JWTSecret))
+	userPrivateRouter := app.Group("/api/secure/user")
 	userPrivateRouter.Get("/profile", handler.GetUserProfile)
 	userPrivateRouter.Put("/profile", handler.UpdateUserProfile)
 	userPrivateRouter.Get("/preferences", handler.GetUserPreferences)
 	userPrivateRouter.Put("/preferences", handler.UpdateUserPreferences)
 
 	// Content Interaction
-	// contentPrivateRouter := app.Group("/api/content", middleware.JWTProtected(config.JWTSecret))
+	// contentPrivateRouter := app.Group("/api/content", middleware.JWTProtected)
 	// contentPrivateRouter.Post("/:contentId/like", handlers.LikeContent)
 	// contentPrivateRouter.Post("/:contentId/dislike", handlers.DislikeContent)
 	// contentPrivateRouter.Post("/:contentId/bookmark", handlers.BookmarkContent)
