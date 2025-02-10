bc20b399b4d3531fb5d84c654621425563c071e0
ubergoose <anisulhaque9391@gmail.com>
2025-02-10 00:45:34 -0500

    style: Format Feedback model for improved readability

--- internal/models/feedback.go
+++ internal/models/feedback.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Feedback struct {
 	gorm.Model
-	UserID      uint
-	ContentID   uint
-	Feedback    string // e.g., "positive", "negative", "neutral"
+	UserID    uint
+	ContentID uint
+	Feedback  string // e.g., "positive", "negative", "neutral"
 }
