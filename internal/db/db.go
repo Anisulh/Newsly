669eb77f40b08a18bc8af3de2bd5a8c77a7f887b
ubergoose <anisulhaque9391@gmail.com>
2025-02-10 00:41:53 -0500

    feat: Implement category initialization during database setup and add logging

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,23 +2,64 @@ package db
 
 import (
 	"Newsly/internal/models"
+	"log"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
 func migrate(db *gorm.DB) error {
 	// Migrate the schema
-	return db.AutoMigrate(&models.Keyword{}, &models.User{}, &models.Content{} )
+	return db.AutoMigrate(&models.User{}, &models.Category{})
+}
+
+func initializeCategories(db *gorm.DB) error {
+	log.Print("Initializing categories")
+
+	categories := []models.Category{
+		{Key: "ml"},
+		{Key: "quantum"},
+		{Key: "neuroscience"},
+		{Key: "genetics"},
+		{Key: "renewables"},
+		{Key: "astrophysics"},
+		{Key: "robotics"},
+		{Key: "biotech"},
+		{Key: "blockchain"},
+		{Key: "materials"},
+		{Key: "medicine"},
+		{Key: "social"},
+		{Key: "engineering"},
+		{Key: "cs"},
+		{Key: "data_science"},
+		{Key: "economics"},
+	}
+	for _, cat := range categories {
+		// If a category with this key exists, FirstOrCreate does nothing.
+		if err := db.Where("key = ?", cat.Key).FirstOrCreate(&cat).Error; err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func setupDatabase(connectionString string) (*gorm.DB, error) {
+	log.Print("Setting up database")
 	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
+
 	err = migrate(db)
 	if err != nil {
 		return nil, err
 	}
+	log.Print("Database migration successful")
+	err = initializeCategories(db)
+	if err != nil {
+		return nil, err
+	}
+	log.Print("Database initialization successful")
 	return db, nil
 }
 
